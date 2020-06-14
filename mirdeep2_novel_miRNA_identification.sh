35f7fea6f3c235cf6104bb68736120cad701d703
MaHulliger <66909693+MaHulliger@users.noreply.github.com>
2020-06-14 15:38:54 +0200

    Create mirdeep2_novel_miRNA_identification.sh
    MiRDeep2 script used to identify putative novel equine miRNAs.

---
+++ mirdeep2_novel_miRNA_identification.sh
@@ -0,0 +1,4 @@
+module add SequenceAnalysis/RNAalignmentFolding/miRDeep2/0.0.7;
+
+
+miRDeep2.pl ALL_LUNG_collapsed.fa EquCab3_noWS_chr_NW.fa ALL_LUNGvsGenome.arf Eca_mature.fa Human_mature.fa precursors_Horse.fa -t Eca -P
